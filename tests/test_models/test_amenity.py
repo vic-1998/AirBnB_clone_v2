5987c6f9ee221d726a7fda4300297e6170a19223
vic-1998 <victor.mplaza98@gmail.com>
2021-03-28 14:55:14 -0500

    Add folder tests.

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,100 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""
+Contains Amenity classes
+"""
+import unittest
+from datetime import datetime
+import inspect
+from models.base_model import BaseModel
 from models.amenity import Amenity
+from models import amenity
+import pep8
+amenity = amenity.Amenity
 
 
-class test_Amenity(test_basemodel):
-    """ """
+class TestAmenityDocs(unittest.TestCase):
+    """Tests to check the documentation and style of Amenity class"""
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the doc tests"""
+        cls.amenity_f = inspect.getmembers(amenity, inspect.isfunction)
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Amenity"
-        self.value = Amenity
+    def test_pep8_conformance_Amenity(self):
+        """Test that models/Amenity.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['models/amenity.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def test_name2(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+    def test_pep8_conformance_test_Amenity(self):
+        """Test that tests/test_models/test_Amenity.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_models/test_amenity.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_Amenity_module_docstring(self):
+        """Test for the Amenity.py module docstring"""
+        self.assertIsNot(amenity.__doc__, None,
+                         "Amenity.py needs a docstring")
+        self.assertTrue(len(amenity.__doc__) >= 1,
+                        "Amenity.py needs a docstring")
+
+    def test_Amenity_class_docstring(self):
+        """Test for the City class docstring"""
+        self.assertIsNot(amenity.__doc__, None,
+                         "Amenity class needs a docstring")
+        self.assertTrue(len(amenity.__doc__) >= 1,
+                        "Amenity class needs a docstring")
+
+    def test_Amenity_func_docstrings(self):
+        """Test for the presence of docstrings in Amenity methods"""
+        for func in self.amenity_f:
+            self.assertIsNot(func[1].__doc__, None,
+                             "{:s} method needs a docstring".format(func[0]))
+            self.assertTrue(len(func[1].__doc__) >= 1,
+                            "{:s} method needs a docstring".format(func[0]))
+
+
+class TestAmenity(unittest.TestCase):
+    """Test the Amenity class"""
+
+    def test_is_subclass(self):
+        """Test that Amenity is a subclass of BaseModel"""
+        amenity = Amenity()
+        self.assertIsInstance(amenity, BaseModel)
+        self.assertTrue(hasattr(amenity, "id"))
+        self.assertTrue(hasattr(amenity, "created_at"))
+        self.assertTrue(hasattr(amenity, "updated_at"))
+
+    def test_last_name_attr(self):
+        """Test that Amenity has attr last_name, and it's an empty string"""
+        amenity = Amenity()
+        self.assertTrue(hasattr(amenity, "name"))
+        self.assertEqual(amenity.name, "")
+
+    def test_to_dict_creates_dict(self):
+        """test to_dict method creates a dictionary with proper attrs"""
+        u = Amenity()
+        new_d = u.to_dict()
+        self.assertEqual(type(new_d), dict)
+        for attr in u.__dict__:
+            self.assertTrue(attr in new_d)
+            self.assertTrue("__class__" in new_d)
+
+    def test_to_dict_values(self):
+        """test that values in dict returned from to_dict are correct"""
+        t_format = "%Y-%m-%dT%H:%M:%S.%f"
+        u = Amenity()
+        new_d = u.to_dict()
+        self.assertEqual(new_d["__class__"], "Amenity")
+        self.assertEqual(type(new_d["created_at"]), str)
+        self.assertEqual(type(new_d["updated_at"]), str)
+        self.assertEqual(new_d["created_at"], u.created_at.strftime(t_format))
+        self.assertEqual(new_d["updated_at"], u.updated_at.strftime(t_format))
+
+    def test_str(self):
+        """test that the str method has the correct output"""
+        amenity = Amenity()
+        string = "[Amenity] ({}) {}".format(amenity.id, amenity.__dict__)
+        self.assertEqual(string, str(amenity))
--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,24 +1,106 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""
+Contains City classes
+"""
+import unittest
+from datetime import datetime
+import inspect
+from models.base_model import BaseModel
 from models.city import City
+from models import city
+import pep8
+City = city.City
 
 
-class test_City(test_basemodel):
-    """ """
+class TestCityDocs(unittest.TestCase):
+    """Tests to check the documentation and style of City class"""
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the doc tests"""
+        cls.city_f = inspect.getmembers(City, inspect.isfunction)
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "City"
-        self.value = City
+    def test_pep8_conformance_City(self):
+        """Test that models/City.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['models/city.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def test_state_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.state_id), str)
+    def test_pep8_conformance_test_City(self):
+        """Test that tests/test_models/test_City.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_models/test_city.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def test_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+    def test_City_module_docstring(self):
+        """Test for the City.py module docstring"""
+        self.assertIsNot(city.__doc__, None,
+                         "City.py needs a docstring")
+        self.assertTrue(len(city.__doc__) >= 1,
+                        "City.py needs a docstring")
+
+    def test_City_class_docstring(self):
+        """Test for the City class docstring"""
+        self.assertIsNot(city.__doc__, None,
+                         "City class needs a docstring")
+        self.assertTrue(len(city.__doc__) >= 1,
+                        "City class needs a docstring")
+
+    def test_City_func_docstrings(self):
+        """Test for the presence of docstrings in City methods"""
+        for func in self.city_f:
+            self.assertIsNot(func[1].__doc__, None,
+                             "{:s} method needs a docstring".format(func[0]))
+            self.assertTrue(len(func[1].__doc__) >= 1,
+                            "{:s} method needs a docstring".format(func[0]))
+
+
+class TestCity(unittest.TestCase):
+    """Test the City class"""
+
+    def test_is_subclass(self):
+        """Test that City is a subclass of BaseModel"""
+        city = City()
+        self.assertIsInstance(city, BaseModel)
+        self.assertTrue(hasattr(city, "id"))
+        self.assertTrue(hasattr(city, "created_at"))
+        self.assertTrue(hasattr(city, "updated_at"))
+
+    def test_last_name_attr(self):
+        """Test that City has attr state_id, and it's an empty string"""
+        city = City()
+        self.assertTrue(hasattr(city, "state_id"))
+        self.assertEqual(city.state_id, "")
+
+    def test_last_name_attr(self):
+        """Test that City has attr name, and it's an empty string"""
+        city = City()
+        self.assertTrue(hasattr(city, "name"))
+        self.assertEqual(city.name, "")
+
+    def test_to_dict_creates_dict(self):
+        """test to_dict method creates a dictionary with proper attrs"""
+        u = City()
+        new_d = u.to_dict()
+        self.assertEqual(type(new_d), dict)
+        for attr in u.__dict__:
+            self.assertTrue(attr in new_d)
+            self.assertTrue("__class__" in new_d)
+
+    def test_to_dict_values(self):
+        """test that values in dict returned from to_dict are correct"""
+        t_format = "%Y-%m-%dT%H:%M:%S.%f"
+        u = City()
+        new_d = u.to_dict()
+        self.assertEqual(new_d["__class__"], "City")
+        self.assertEqual(type(new_d["created_at"]), str)
+        self.assertEqual(type(new_d["updated_at"]), str)
+        self.assertEqual(new_d["created_at"], u.created_at.strftime(t_format))
+        self.assertEqual(new_d["updated_at"], u.updated_at.strftime(t_format))
+
+    def test_str(self):
+        """test that the str method has the correct output"""
+        city = City()
+        string = "[City] ({}) {}".format(city.id, city.__dict__)
+        self.assertEqual(string, str(city))
