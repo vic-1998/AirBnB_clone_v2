7d186228f33ceb4f9e7bb54533fbb98618d40322
vic-1998 <victor.mplaza98@gmail.com>
2021-03-29 16:50:50 -0500

    Add state and city.

--- models/city.py
+++ models/city.py
@@ -1,13 +1,34 @@
 #!/usr/bin/python3
-'''define class city'''
+"""define class city"""
+import models
 from models.base_model import BaseModel
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel):
-    '''class City'''
-    state_id = ""
-    name = ""
+    """class City"""
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities")
+    else:
+        state_id = ""
+        name = ""
 
     def __init__(self, *args, **kwargs):
-        '''constructor method'''
+        """initializes city"""
         super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
--- models/state.py
+++ models/state.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
-'''define class state'''
-from models.base_model import BaseModel
+"""define class state"""
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
-    '''class state'''
-
-    name = ""
+    """class state"""
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
 
     def __init__(self, *args, **kwargs):
-        '''constructor method'''
+        """initializes state"""
         super().__init__(*args, **kwargs)
