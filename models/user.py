e5ed24a3af9961d050bfb619f8e2cf8fc466027f
nicolasherrerac <nicolasherrerac20@gmail.com>
2021-03-30 18:37:09 -0500

    Delete the last lines

--- models/user.py
+++ models/user.py
@@ -17,5 +17,3 @@ class User(BaseModel, Base):
     last_name = Column(String(128), nullable=True)
     places = relationship('Place', backref='user', cascade='all, delete')
     reviews = relationship('Review', backref='user', cascade='all, delete')
-    places = relationship("Place", backref="user", cascade="all, delete")
-    reviews = relationship("Review", backref="user", cascade="all, delete")
