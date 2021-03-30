6b50bcff20a0bd390e7b8a1e5685b343d7e895e7
nicolasherrerac <nicolasherrerac20@gmail.com>
2021-03-30 16:59:17 -0500

    update state

--- models/state.py
+++ models/state.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python
 """Contains class State"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
+from models.city import City
+from os import getenv
+import models
 
 
 class State(BaseModel):
     """The state class"""
-    name = ""
 
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref='state')
+    else:
+        @property
+        def cities(self):
+            """to list the cities"""
+            from models import storage
+            citiesList = []
+            for key, obj in storage.all(City).items():
+                if self.id == obj.state_id:
+                    citiesList.append(obj)
+            return citiesList
