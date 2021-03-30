862026e34537e8c167d62fb942411e965c4beabb
nicolasherrerac <nicolasherrerac20@gmail.com>
2021-03-30 18:51:42 -0500

    Add comment

--- models/review.py
+++ models/review.py
@@ -1,11 +1,12 @@
-#!/usr/bin/python
-"""Contains class Review"""
+#!/usr/bin/python3
+""" Review module for the HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey
 
 
 class Review(BaseModel, Base):
-    __tablename__ = 'reviews'
+    """ Review class to store review information"""
+    __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
