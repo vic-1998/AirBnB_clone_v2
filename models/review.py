d28df10bbdcb46f5542e91aa78641fe364580238
nicolasherrerac <nicolasherrerac20@gmail.com>
2021-03-30 17:28:10 -0500

    try new changes

--- models/review.py
+++ models/review.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python
 """Contains class Review"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
-class Review(BaseModel):
-    """The Review class"""
-    place_id = ""
-    user_id = ""
-    text = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Review"""
-        super().__init__(*args, **kwargs)
+class Review(BaseModel, Base):
+    __tablename__ = 'reviews'
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
