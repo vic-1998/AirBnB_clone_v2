ccb7411aac8703a87bc38bc1e501bd3780789e10
nicolasherrerac <nicolasherrerac20@gmail.com>
2021-03-30 09:41:56 -0500

    Update function

--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import backref, relationship
 
 class User(BaseModel, Base):
     '''Class user'''
@@ -11,3 +12,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
+    places = relationship("Place" backref="user", cascade="all, delete")
