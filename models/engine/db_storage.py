ae3dd1d6bd9e6c3cb5b8bbfbacfca66e58e8d69c
vic-1998 <victor.mplaza98@gmail.com>
2021-05-01 08:37:17 -0500

    Update methos in filestorage and de_storage.

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -73,4 +73,4 @@ class DBStorage:
 
     def close(self):
         """call remove() method on the private session attribute"""
-        self.__session.close()
+        self.__session.remove()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,3 +65,7 @@ class FileStorage():
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
                 del self.__objects[key]
+
+    def close(self):
+        """method for deserializing the JSON"""
+        self.reload()
